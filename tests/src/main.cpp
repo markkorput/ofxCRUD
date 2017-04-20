230db8f1e7f4773a34271dced83b79aba3d55f25
Mark van de Korput <mark.korput@gmail.com>
2017-04-20 22:01:27 +0200

    first tests for Parameter class

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,21 +1,37 @@
 #include "ofxUnitTests.h"
 #include "ofxCRUD.h"
 
-#define TEST_START(x) {ofLog()<<"x";
+#define TEST_START(x) {ofLog()<<#x;
 #define TEST_END }
 
+using namespace ofxCRUD;
+
 class ofApp: public ofxUnitTestsApp{
-  void run(){
-      TEST_START(nothin specific)
-      TEST_END
-  }
+
+    void run(){
+        TEST_START(Parameter::getEvent)
+            Parameter<int> number;
+
+            // normal ofParameter behaviour
+            number.set(123);
+            test_eq(number.get(), 123, "");
+            test_eq(number.get(), 123, "");
+
+            // getEvent
+            number.getEvent.addListener([](int& Parameter<int> numberParam)){
+                // change param value before it respond to the next .get call
+                numberParam.set(456);
+            });
+
+            test_eq(number.get(), 456, "");
+        TEST_END
+    }
 };
 
 
 #include "ofAppNoWindow.h"
 #include "ofAppRunner.h"
 
-//========================================================================
 int main( ){
   ofInit();
   auto window = std::make_shared<ofAppNoWindow>();
